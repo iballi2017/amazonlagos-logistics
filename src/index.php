878e47efc197208c3edaa55e0ec1318de2066a3d
iballi2017 <iballi201@gmail.com>
2024-10-23 19:26:00 +0100

    starter files

---
+++ src/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Amazon Lagos</title>
+    <!-- tailwindcss -->
+    <link rel="stylesheet" href="./stylesheets/tailwindcss/output.css">
+    <!-- main styles -->
+    <link rel="stylesheet" href="./stylesheets/main.css">
+</head>
+
+<body>
+    <h1 class="text-4xl font-bold">Hello World!!</h1>
+
+
+    <div class="bg-on-theme-clr-light-100 text-red-100">
+        Hello Programmer!
+    </div>
+    <div class="bg-on-theme-clr-primary text-red-100">
+        Hello Programmer!
+    </div>
+</body>
+
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,php}"],
+  theme: {
+    extend: {},
+    colors: {
+      "theme-clr-primary": 'hsl(var(--theme-clr-primary) / <alpha-value>)',
+      "on-theme-clr-primary": 'hsl(var(--on-theme-clr-primary) / <alpha-value>)',
+      "theme-clr-secondary": 'hsl(var(--theme-clr-secondary) / <alpha-value>)',
+      "on-theme-clr-secondary": 'hsl(var(--on-theme-clr-secondary) / <alpha-value>)',
+      "theme-clr-light": 'hsl(var(--theme-clr-light) / <alpha-value>)',
+      "on-theme-clr-light": 'hsl(var(on-theme-clr-light) / <alpha-value>)',
+      "theme-clr-light-100": 'hsl(var(--theme-clr-light-100) / <alpha-value>)',
+      "on-theme-clr-light-100": 'hsl(var(--on-theme-clr-light-100) / <alpha-value>)',
+    }
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
+
