3d48620668a684700652187c06866716b57b177e
ibrahim <ibrahim.alli@mysourcebank.com>
2024-10-25 13:46:48 +0100

    update on landing page

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <!-- head -->
-<?php include "./includes/head.php" ?>
+<?php include __DIR__ . "/includes/head.php" ?>
 <!-- top header -->
 <div class="bg-theme-clr-light text-on-theme-clr-light">
     <ul class="main-container | flex flex-wrap items-center justify-between text-sm py-3">
@@ -14,7 +14,7 @@
     </ul>
 </div>
 
-<?php include "./components/header.php" ?>
+<?php include __DIR__ . "/components/header.php" ?>
 
 
 <section id="hero">
@@ -62,12 +62,16 @@
             <h2 class="inline-block bg-theme-clr-secondary text-on-theme-clr-secondary rounded-r-full rounded-l-full
             font-semibold px-4 py-1.5 uppercase mb-4">We are AmazonLagos</h2>
             <p class="text-[1.8rem] font-extrabold uppercase">Introducing The most Modern way of Transportation</p>
-            <p class="mb-3">Welcome to AmazonLagos, where we are dedicated to transforming the online shopping experience for Nigerians. Our mission is to bridge the gap between Nigeria and leading global e-commerce platforms such as Amazon, Alibaba, and AliExpress. </p>
+            <p class="mb-3">Welcome to AmazonLagos, where we are dedicated to transforming the online shopping
+                experience for Nigerians. Our mission is to bridge the gap between Nigeria and leading global e-commerce
+                platforms such as Amazon, Alibaba, and AliExpress. </p>
             <a href="#"
-                class="rounded-md px-3 xl:px-6 py-3 inline-block text-xs font-bold uppercase bg-theme-clr-primary text-on-theme-clr-primary">Read More</a>
+                class="rounded-md px-3 xl:px-6 py-3 inline-block text-xs font-bold uppercase bg-theme-clr-primary text-on-theme-clr-primary">Read
+                More</a>
         </div>
         <div class="col-span-12 lg:col-span-6 flex place-content-center">
-            <img src="./assets/images/iIntroducing-the-most-modern-way-of-transportation.png" alt="Introducing The most Modern way of Transportation">
+            <img src="./assets/images/iIntroducing-the-most-modern-way-of-transportation.png"
+                alt="Introducing The most Modern way of Transportation">
         </div>
     </div>
 </section>
@@ -85,10 +89,13 @@
             <div class="col-span-12 md:col-span-6 lg:col-span-3">
                 <div class="flex flex-col items-center gap-4 py-10 px-6 bg-white rounded-lg shadow h-full">
                     <div>
-                        <img src="./assets/images/seamless-access-to-global-markets.png" alt="Seamless Access to Global Markets">
+                        <img src="./assets/images/seamless-access-to-global-markets.png"
+                            alt="Seamless Access to Global Markets">
                     </div>
-                    <h6 class="text-theme-clr-secondary font-bold">Seamless Access<br class="hidden lg:block" /> to Global Markets</h6>
-                    <p class="text-on-theme-clr-light">We offer Nigerians easy access to international e-commerce sites with a diverse range of tailored products.</p>
+                    <h6 class="text-theme-clr-secondary font-bold">Seamless Access<br class="hidden lg:block" /> to
+                        Global Markets</h6>
+                    <p class="text-on-theme-clr-light">We offer Nigerians easy access to international e-commerce sites
+                        with a diverse range of tailored products.</p>
                 </div>
             </div>
             <div class="col-span-12 md:col-span-6 lg:col-span-3">
@@ -96,26 +103,34 @@
                     <div>
                         <img src="./assets/images/flexible-payment-options.png" alt="Seamless Access to Global Markets">
                     </div>
-                    <h6 class="text-theme-clr-secondary font-bold">Flexible <br class="hidden lg:block" /> Payment Options</h6>
-                    <p class="text-on-theme-clr-light">Our platform allows for easy payments in Naira or with dollar cards, simplifying the shopping process.</p>
+                    <h6 class="text-theme-clr-secondary font-bold">Flexible <br class="hidden lg:block" /> Payment
+                        Options</h6>
+                    <p class="text-on-theme-clr-light">Our platform allows for easy payments in Naira or with dollar
+                        cards, simplifying the shopping process.</p>
                 </div>
             </div>
             <div class="col-span-12 md:col-span-6 lg:col-span-3">
                 <div class="flex flex-col items-center gap-4 py-10 px-6 bg-white rounded-lg shadow h-full">
                     <div>
-                        <img src="./assets/images/reliable-logistics-and-delivery.png" alt="Seamless Access to Global Markets">
+                        <img src="./assets/images/reliable-logistics-and-delivery.png"
+                            alt="Seamless Access to Global Markets">
                     </div>
-                    <h6 class="text-theme-clr-secondary font-bold">Reliable <br class="hidden lg:block" /> Logistics and Delivery</h6>
-                    <p class="text-on-theme-clr-light">We ensure efficient shipping from our U.S. warehouse directly to Nigerian addresses, prioritizing timely delivery.</p>
+                    <h6 class="text-theme-clr-secondary font-bold">Reliable <br class="hidden lg:block" /> Logistics and
+                        Delivery</h6>
+                    <p class="text-on-theme-clr-light">We ensure efficient shipping from our U.S. warehouse directly to
+                        Nigerian addresses, prioritizing timely delivery.</p>
                 </div>
             </div>
             <div class="col-span-12 md:col-span-6 lg:col-span-3">
                 <div class="flex flex-col items-center gap-4 py-10 px-6 bg-white rounded-lg shadow h-full">
                     <div>
-                        <img src="./assets/images/dedicated-cCustomer-support.png" alt="Seamless Access to Global Markets">
+                        <img src="./assets/images/dedicated-cCustomer-support.png"
+                            alt="Seamless Access to Global Markets">
                     </div>
-                    <h6 class="text-theme-clr-secondary font-bold">Dedicated <br class="hidden lg:block" /> Customer Support</h6>
-                    <p class="text-on-theme-clr-light">Our customer service team is always available to assist, ensuring a smooth and supportive shopping experience.</p>
+                    <h6 class="text-theme-clr-secondary font-bold">Dedicated <br class="hidden lg:block" /> Customer
+                        Support</h6>
+                    <p class="text-on-theme-clr-light">Our customer service team is always available to assist, ensuring
+                        a smooth and supportive shopping experience.</p>
                 </div>
             </div>
         </div>
@@ -124,10 +139,11 @@
 
 <section class="py-10 lg:py-20">
     <div class="main-container">
-        <div>
+        <div class="text-center lg:text-left">
             <h2 class="inline-block bg-theme-clr-secondary text-on-theme-clr-secondary rounded-r-full rounded-l-full
             font-semibold px-4 py-1.5 uppercase mb-4">We are AmazonLagos</h2>
-            <p class="text-xl lg:text-[1.8rem] font-extrabold uppercase mb-2">Check Our Wide Variety of Logistics<br class="hidden lg:block" /> Services</p>
+            <p class="text-xl lg:text-[1.8rem] font-extrabold uppercase mb-4">Check Our Wide Variety of Logistics<br
+                    class="hidden lg:block" /> Services</p>
 
             <div class="grid grid-cols-2 gap-4 lg:gap-6">
                 <div class="col-span-2 lg:col-span-1">
@@ -137,9 +153,11 @@
                         </div>
                         <div class="col-span-12 lg:col-span-7 flex flex-col lg:block items-center">
                             <h6 class="text-theme-clr-secondary font-bold mb-2">Jombo pack: Buy & Ship</h6>
-                            <p class="text-on-theme-clr-light mb-2">This segment allows customers to shop from international platforms using Naira, eliminating the need for a dollar card.</p>
+                            <p class="text-on-theme-clr-light mb-2">This segment allows customers to shop from
+                                international platforms using Naira, eliminating the need for a dollar card.</p>
                             <a href="#"
-                                class="rounded-md px-3 xl:px-6 py-3 inline-block text-xs font-bold uppercase bg-theme-clr-primary text-on-theme-clr-primary">Get a Quote</a>
+                                class="rounded-md px-3 xl:px-6 py-3 inline-block text-xs font-bold uppercase bg-theme-clr-primary text-on-theme-clr-primary">Get
+                                a Quote</a>
 
                         </div>
                     </div>
@@ -151,9 +169,12 @@
                         </div>
                         <div class="col-span-12 lg:col-span-7 flex flex-col lg:block items-center">
                             <h6 class="text-theme-clr-secondary font-bold mb-2">Ship only</h6>
-                            <p class="text-on-theme-clr-light mb-2">This segment focuses on logistics, enabling customers to ship their purchases to a U.S. warehouse, from where AmazonLagos will handle the shipping to their Nigerian addresses.</p>
+                            <p class="text-on-theme-clr-light mb-2">This segment focuses on logistics, enabling
+                                customers to ship their purchases to a U.S. warehouse, from where AmazonLagos will
+                                handle the shipping to their Nigerian addresses.</p>
                             <a href="#"
-                                class="rounded-md px-3 xl:px-6 py-3 inline-block text-xs font-bold uppercase bg-theme-clr-primary text-on-theme-clr-primary">Get a Quote</a>
+                                class="rounded-md px-3 xl:px-6 py-3 inline-block text-xs font-bold uppercase bg-theme-clr-primary text-on-theme-clr-primary">Get
+                                a Quote</a>
 
                         </div>
                     </div>
@@ -165,16 +186,29 @@
 
 <section class="py-10 bg-theme-clr-secondary text-on-theme-clr-secondary">
     <div class="main-container">
-        <div class="grid grid-cols-12">
-            <div class="col-span-12 lg:col-span-6">
-                <p class="ext-xl lg:text-[1.8rem] font-extrabold uppercase text-center">We guarantee secure transportation and reliable delivery.</p>
+        <div class="grid grid-cols-12 items-center">
+            <div class="col-span-12 lg:col-span-5">
+                <p class="ext-xl lg:text-[1.8rem] font-extrabold uppercase text-center">We guarantee secure
+                    transportation and reliable delivery.</p>
             </div>
-            <div class="col-span-12 lg:col-span-6">
+            <div class="col-span-12 lg:col-span-3">A</div>
+            <div class="col-span-12 lg:col-span-4 text-center lg:text-left">
+                <a href="#" class="bg-white hover:bg-theme-clr-primary focus:bottom-1 focus:border-theme-clr-primary whitespace-nowrap inline-flex gap-x-4 items-center text-on-theme-clr-primary hover:text-white font-semibold px-6 py-2.5 rounded-r-full rounded-l-full">
+                    <img src="./assets/icons/icon-ph_phone-fill.svg" alt="Call us">
+                    <span>234 1723999-99-2</span>
+                </a>
             </div>
         </div>
     </div>
 </section>
 
 
+<footer class="bg-theme-clr-light">
+    <div class="main-container">
+        Hello
+    </div>
+</footer>
+
+
 <!-- foot -->
-<?php include "./includes/foot.php" ?>
+<?php include __DIR__ . "/includes/foot.php" ?>
