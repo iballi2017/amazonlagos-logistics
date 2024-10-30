8997912170afd2737fc50ce07da4f5e36cd830b4
iballi2017 <iballi201@gmail.com>
2024-10-31 00:09:31 +0100

    update

--- src/change-password.php
+++ src/change-password.php
@@ -1,1 +1,46 @@
-<h1>change-password</h1>
+<h2 class="p-4 text-lg font-bold bg-theme-clr-light-100 text-theme-clr-secondary">Change Password</h2>
+
+<div class="relative overflow-x-auto text-theme-clr-secondary">
+    <div class="p-4">
+        <form action="">
+            <div class="grid grid-cols-2 gap-2 lg:gap-4">
+                <div class="col-span-2 lg:col-span-1">
+                    <label for="password" class="block mb-2 font-medium text-theme-clr-secondary cursor-pointer">Old Password</label>
+                    <div class="relative">
+                        <button type="button" class="password-type-toggler | absolute right-2 top-2 opacity-40">
+                            <img src="./assets/icons/icon-eye.svg" alt="show">
+                            <span class="sr-only">Toggle</span>
+                            <img src="./assets/icons/icon-eye-off.svg" alt="hide" class="hidden">
+                        </button>
+                        <input type="password" id="password" class="border border-theme-clr-light-800 text-theme-clr-secondary text-sm rounded-lg focus:ring-theme-clr-primary focus:border-theme-clr-primary block w-full p-2.5 py-3" required />
+                    </div>
+                </div>
+                <div class="col-span-2 lg:col-span-1">
+                    <label for="password" class="block mb-2 font-medium text-theme-clr-secondary cursor-pointer">New Password</label>
+                    <div class="relative">
+                        <button type="button" class="password-type-toggler | absolute right-2 top-2 opacity-40">
+                            <img src="./assets/icons/icon-eye.svg" alt="show">
+                            <span class="sr-only">Toggle</span>
+                            <img src="./assets/icons/icon-eye-off.svg" alt="hide" class="hidden">
+                        </button>
+                        <input type="password" id="password" class="border border-theme-clr-light-800 text-theme-clr-secondary text-sm rounded-lg focus:ring-theme-clr-primary focus:border-theme-clr-primary block w-full p-2.5 py-3" required />
+                    </div>
+                </div>
+                <div class="col-span-2 lg:col-span-1">
+                    <label for="password" class="block mb-2 font-medium text-theme-clr-secondary cursor-pointer">Confirm New Password</label>
+                    <div class="relative">
+                        <button type="button" class="password-type-toggler | absolute right-2 top-2 opacity-40">
+                            <img src="./assets/icons/icon-eye.svg" alt="show">
+                            <span class="sr-only">Toggle</span>
+                            <img src="./assets/icons/icon-eye-off.svg" alt="hide" class="hidden">
+                        </button>
+                        <input type="password" id="password" class="border border-theme-clr-light-800 text-theme-clr-secondary text-sm rounded-lg focus:ring-theme-clr-primary focus:border-theme-clr-primary block w-full p-2.5 py-3" required />
+                    </div>
+                </div>
+                <div class="col-span-2">
+
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
