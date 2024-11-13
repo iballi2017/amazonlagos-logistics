c5ef10b6f52c24885a906e6259024b76a5f8314f
ibrahim <ibrahim.alli@mysourcebank.com>
2024-11-14 00:18:27 +0100

    update

--- src/tracking-status.php
+++ src/tracking-status.php
@@ -2,7 +2,7 @@
 <div class="p-4">
     <div class="relative">
         <ul class="tracking-status-list">
-            <li class="status passed | relative border-l-2 pl-4 lg:pl-6 before:content-['gg'] before:h-[16px] before:w-[16px] before:absolute before:-left-[8px]">
+            <li class="status passed | relative pl-5 lg:pl-8 before:content-['gg'] before:h-[16px] before:w-[16px] before:absolute before:-left-[8px]">
                 <div class="flex items-center gap-2">
                     <span class="date">2023-04-09</span>
                     <span class="time">20:22:27</span>
@@ -11,16 +11,49 @@
                     [AmazonLagos warehouse] [InternationalLogistics] processing, order has been inbounded
                 </p>
             </li>
-            <li class="status current | relative border-l-2 pl-4 lg:pl-6 before:content-['gg'] before:h-[16px] before:w-[16px] before:absolute before:-left-[8px]">
+            <li class="status passed | relative pl-5 lg:pl-8 before:content-['gg'] before:h-[16px] before:w-[16px] before:absolute before:-left-[8px]">
                 <div class="flex items-center gap-2">
                     <span class="date">2023-04-09</span>
                     <span class="time">20:22:27</span>
                 </div>
                 <p class="text-theme-clr-secondary">
-                    [AmazonLagos warehouse] [InternationalLogistics] processing, order has been inbounded
+                    [AmazonLagos warehouse] [InternationalLogistics] processing assembling operation
+                </p>
+            </li>
+            <li class="status current | relative pl-5 lg:pl-8 before:content-['gg'] before:h-[16px] before:w-[16px] before:absolute before:-left-[8px]">
+                <div class="flex items-center gap-2">
+                    <span class="date">2023-04-09</span>
+                    <span class="time">20:22:27</span>
+                </div>
+                <p class="text-theme-clr-secondary">
+                    [AmazonLagos warehouse] [InternationalLogistics] processing, order has been outbounded
+                </p>
+            </li>
+            <li class="status | relative pl-5 lg:pl-8 before:content-['gg'] before:h-[16px] before:w-[16px] before:absolute before:-left-[8px]">
+                <div class="flex items-center gap-2">
+                    <!-- <span class="date">2023-04-09</span>
+                    <span class="time">20:22:27</span> -->
+                </div>
+                <p class="text-theme-clr-secondary">
+                    Clearance in progress
+                </p>
+            </li>
+            <li class="status | relative pl-5 lg:pl-8 before:content-['gg'] before:h-[16px] before:w-[16px] before:absolute before:-left-[8px]">
+                <div class="flex items-center gap-2">
+                    <!-- <span class="date">2023-04-09</span>
+                    <span class="time">20:22:27</span> -->
+                </div>
+                <p class="text-theme-clr-secondary">
+                    Delivered
                 </p>
             </li>
         </ul>
+
+        <button type="button" class="bg-[#DEE3C3] text-on-theme-clr-primary font-bold block w-full p-4 rounded-lg mt-6">
+
+            <div>More Information</div>
+            <div>Collapse Information</div>
+        </button>
 
     </div>
 </div>
